152fd6879ff452bc4bb086e6ab380462a5b017f5
rdmckinnon <ryan@ryanmckinnon.com>
2014-11-10 18:33:00 -0500

    Last

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,8 @@ class Recipe < ActiveRecord::Base
 	validates :minutes_to_prepare, presence: true, numericality: { greater_than: 1 }
 	validates :minutes_to_cook, presence: true, numericality: { greater_than: 1 }
 
+	mount_uploader :picture, PictureUploader
+
 
 	def minutes_total
 		minutes_to_prepare + minutes_to_cook 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106005802) do
+ActiveRecord::Schema.define(version: 20141110231834) do
 
   create_table "recipes", force: true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141106005802) do
     t.integer  "minutes_to_cook"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
 end
