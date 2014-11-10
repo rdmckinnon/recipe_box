f0b85251e98fb0c0ba3515b1834716ad184d689c
rdmckinnon <ryan@ryanmckinnon.com>
2014-11-10 18:16:53 -0500

    Image

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,11 @@
 class Recipe < ActiveRecord::Base
 
-	validates :name, presence: true, length: { minimum: 2}
-	validates :category, presence: true, length: { minimum: 3}
-	validates :ingredients, presence: true, length: { minimum: 3}
-	validates :instructions, presence: true, length: { maximum: 1500}
-	validates :minutes_to_prepare, presence: true, length: { greater_than: 0}
-	validates :minutes_to_cook, presence: true, length: { greater_than: 0}
+	validates :name, presence: true, length: { minimum: 2 }
+	validates :category, presence: true, length: { minimum: 3 }
+	validates :ingredients, presence: true, length: { minimum: 3 }
+	validates :instructions, presence: true, length: { maximum: 1500 }
+	validates :minutes_to_prepare, presence: true, numericality: { greater_than: 1 }
+	validates :minutes_to_cook, presence: true, numericality: { greater_than: 1 }
 
 
 	def minutes_total
